ccd04ebcd9d2986181e1a87f0648ecf086c05d80
Berkay <yurdaguel.b@gmail.com>
2020-09-15 14:00:25 +0200

    backup of boundaries,found right tiles

--- src/components/map/map.component.jsx
+++ src/components/map/map.component.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react"
-import { Map, TileLayer, GeoJSON } from "react-leaflet"
+import { Map, TileLayer, GeoJSON, MapLayer } from "react-leaflet"
 import "leaflet-boundary-canvas"
 import L from "leaflet"
 
 //utils
 import geojson from "./utils/geojson"
+import { border } from "./utils/border"
 
 //styles
 import "./map.styles.scss"
@@ -12,11 +13,10 @@ import "./map.styles.scss"
 export default class MyMap extends Component {
   componentDidMount() {
     L.TileLayer.boundaryCanvas(
-      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      "https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png",
       {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        boundary: geojson,
       }
     ).addTo(this.map.leafletElement)
   }
@@ -27,7 +27,7 @@ export default class MyMap extends Component {
     if (typeof window !== "undefined") {
       return (
         <Map center={position} zoom={5} ref={Map => (this.map = Map)}>
-          <GeoJSON data={geojson} />
+          <GeoJSON data={border} />
         </Map>
       )
     }
