1dcf09d3097fb5fca26b38face6143e735ccdf0d
Berkay <yurdaguel.b@gmail.com>
2020-09-24 20:51:20 +0200

    working on util function for fetching youtube data

--- src/components/video-container/utils/utils.js
+++ src/components/video-container/utils/utils.js
@@ -3,28 +3,53 @@ const getYoutubeData = async id => {
     `https://www.googleapis.com/youtube/v3/videos?id=${id}&part=contentDetails&part=snippet&key=AIzaSyBWyJ8_5gt686cUsIlEymxe7ho9M0I56TY`
   )
   const json = await res.json()
-  console.log(json)
+  const data = json.items[0]
+  //console.log(data)
+  let time = ""
+  if (data) {
+    time = data.contentDetails.duration
+      .replace("PT", "")
+      .replace("H", ":")
+      .replace("M", ":")
+      .replace("S", "")
+      .split(":")
+    for (let i = 0; i < time.length; i++) {
+      if (time[i].length == 1) time[i] = "0" + time[i]
+    }
+    time = time.join(":")
+  }
+  let description = ""
+  if (data) description = data.snippet.description
+
+  return [time, description]
 }
 
 export const getData = routes => {
-  const videos = {}
-  let cnt = 0
+  const videos = []
 
   for (let i of routes) {
     if (i["_popup"] && i["_popup"]["_content"]) {
-      let title = i["_popup"]["_content"].match(/(?<=<h2>)(.*)(?=<\/h2>)/gm)[0]
-      console.log(i["_popup"]["_content"])
+      //let description = i["_popup"]["_content"].match(/(?<=v=)[-\w]{11}/gm)
+      //console.log(i["_popup"]["_content"])
+      let h1 = i["_popup"]["_content"].match(/(?<=<h2>)(.*)(?=<\/h2>)/gm)[0]
       let id = i["_popup"]["_content"].match(/(?<=v=)[-\w]{11}/gm)
-      let description = i["_popup"]["_content"].match(/(?<=v=)[-\w]{11}/gm)
       let km = i["_popup"]["_content"].match(
         /(?<=Distance in km: )([0-9]*[,])?[0-9]+/gm
       )
-      if (km) {
-        console.log(km)
-        console.log(i["_popup"]["_content"])
+      if (km === "") {
+        km = "-"
       }
-      console.log(getYoutubeData(id))
-      let videoLength = ""
+
+      getYoutubeData(id).then(data => {
+        videos.push({
+          h1: h1,
+          id: id,
+          km: km,
+          videoLength: data[0],
+          description: data[1],
+        })
+      })
     }
   }
+  console.log(videos)
 }
