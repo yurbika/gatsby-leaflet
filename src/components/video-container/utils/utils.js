fe5a82d5793e257990ab709d5c10a1466475c020
Berkay <yurdaguel.b@gmail.com>
2020-09-25 19:05:52 +0200

    adding error handling to video component util function and date

--- src/components/video-container/utils/utils.js
+++ src/components/video-container/utils/utils.js
@@ -1,29 +1,37 @@
 const getYoutubeData = async id => {
-  const res = await fetch(
-    `https://www.googleapis.com/youtube/v3/videos?id=${id}&part=contentDetails&part=snippet&key=AIzaSyBWyJ8_5gt686cUsIlEymxe7ho9M0I56TY`
-  )
-  const json = await res.json()
-  const data = json.items[0]
-  //console.log(data)
-  let time = ""
-  let description = ""
-  let date = ""
-  if (data) {
-    time = data.contentDetails.duration
-      .replace("PT", "")
-      .replace("H", ":")
-      .replace("M", ":")
-      .replace("S", "")
-      .split(":")
-    for (let i = 0; i < time.length; i++) {
-      if (time[i].length == 1) time[i] = "0" + time[i]
+  try {
+    const res = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos?id=${id}&part=contentDetails&part=snippet&key=AIzaSyBWyJ8_5gt686cUsIlEymxe7ho9M0I56TY`
+    )
+    const json = await res.json()
+    const data = json.items[0]
+    //console.log(data)
+    let time = ""
+    let description = ""
+    let date = ""
+    if (data) {
+      time = data.contentDetails.duration
+        .replace("PT", "")
+        .replace("H", ":")
+        .replace("M", ":")
+        .replace("S", "")
+        .split(":")
+      for (let i = 0; i < time.length; i++) {
+        if (time[i].length === 1) time[i] = "0" + time[i]
+      }
+      time = time.join(":")
+      description = data.snippet.description
+      date = data.snippet.publishedAt
+        .split("T")[0]
+        .split("-")
+        .reverse()
+        .join("-")
     }
-    time = time.join(":")
-    description = data.snippet.description
-    date = data.snippet.publishedAt.split("T")[0].split("-").reverse().join("-")
-  }
 
-  return [time, description, date]
+    return [time, description, date]
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export const getData = routes => {
