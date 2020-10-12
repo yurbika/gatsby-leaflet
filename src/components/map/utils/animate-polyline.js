d69f68da28809984f62c7807a65628ccb5f282ef
Berkay <yurdaguel.b@gmail.com>
2020-10-12 15:48:25 +0200

    working animation

--- src/components/map/utils/animate-polyline.js
+++ src/components/map/utils/animate-polyline.js
@@ -21,20 +21,13 @@ const animatePolyline = (map, points) => {
     .append("circle")
     .attr("r", 3)
     .attr("class", "waypoints")
+    .style("visibility", "hidden")
 
   var marker = g
     .append("circle")
     .attr("r", 8)
     .attr("id", "marker")
     .attr("class", "travelMarker")
-    .attr(
-      "transform",
-      "translate(" +
-        map.latLngToLayerPoint(points[0]).x +
-        "," +
-        map.latLngToLayerPoint(points[0]).y +
-        ")"
-    )
 
   var linePath = g
     .selectAll(".lineConnect")
@@ -42,6 +35,8 @@ const animatePolyline = (map, points) => {
     .enter()
     .append("path")
     .attr("class", "lineConnect")
+    .style("fill", "none")
+    .style("stroke", "black")
 
   var originANDdestination = [points[0], points[points.length - 1]]
   function applyLatLngToLayer(d) {
@@ -55,8 +50,20 @@ const animatePolyline = (map, points) => {
     .append("circle", ".drinks")
     .attr("r", 5)
     .style("fill", "red")
-    .style("opacity", "1")
-    .attr("transform", function (d) {
+
+  map.on("zoom", reset)
+
+  // this puts stuff on the map!
+  reset()
+  transition()
+
+  function reset() {
+    let sw = L.polyline(points).getBounds()["_southWest"]
+    let ne = L.polyline(points).getBounds()["_northEast"]
+    let bottomRight = map.latLngToLayerPoint(sw)
+    let topLeft = map.latLngToLayerPoint(ne)
+
+    begend.attr("transform", function (d) {
       return (
         "translate(" +
         applyLatLngToLayer(d).x +
@@ -66,22 +73,67 @@ const animatePolyline = (map, points) => {
       )
     })
 
-  let sw = L.polyline(points).getBounds()["_southWest"]
-  let ne = L.polyline(points).getBounds()["_northEast"]
-  let bottomRight = map.latLngToLayerPoint(sw)
-  let topLeft = map.latLngToLayerPoint(ne)
+    ptFeatures.attr("transform", function (d) {
+      return (
+        "translate(" +
+        applyLatLngToLayer(d).x +
+        "," +
+        applyLatLngToLayer(d).y +
+        ")"
+      )
+    })
 
-  svg
-    .attr("width", Math.abs(bottomRight.x - topLeft.x) + 20)
-    .attr("height", Math.abs(bottomRight.y - topLeft.y) + 20)
-    .style("left", bottomRight.x - 10 + "px")
-    .style("top", topLeft.y - 10 + "px")
-    .style("border", "1px solid black")
+    marker.attr(
+      "transform",
+      "translate(" +
+        map.latLngToLayerPoint(points[0]).x +
+        "," +
+        map.latLngToLayerPoint(points[0]).y +
+        ")"
+    )
 
-  g.attr(
-    "transform",
-    "translate(" + (-bottomRight.x + 10) + "," + (-topLeft.y + 10) + ")"
-  )
+    svg
+      .attr("width", Math.abs(bottomRight.x - topLeft.x) + 20)
+      .attr("height", Math.abs(bottomRight.y - topLeft.y) + 20)
+      .style("left", bottomRight.x - 10 + "px")
+      .style("top", topLeft.y - 10 + "px")
+      .style("border", "1px solid black")
+
+    linePath.attr("d", toLine)
+
+    g.attr(
+      "transform",
+      "translate(" + (-bottomRight.x + 10) + "," + (-topLeft.y + 10) + ")"
+    )
+
+    //end reset
+  }
+
+  function transition() {
+    linePath
+      .transition()
+      .duration(7500)
+      .attrTween("stroke-dasharray", tweenDash)
+      .on("end", function () {
+        d3.select(this).call(transition) // infinite loop
+      })
+  }
+
+  function tweenDash() {
+    return function (t) {
+      var l = linePath.node().getTotalLength()
+
+      let interpolate = d3.interpolateString("0," + l, l + "," + l)
+      //t is fraction of time 0-1 since transition began
+      var marker = d3.select("#marker")
+
+      var p = linePath.node().getPointAtLength(t * l)
+
+      //Move the marker to that point
+      marker.attr("transform", "translate(" + p.x + "," + p.y + ")") //move marker
+      return interpolate(t)
+    }
+  } //end tweenDash
 }
 
 export default animatePolyline
