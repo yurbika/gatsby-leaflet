6f8c0e9bf5eba470a7fbef314d426267eeaa1169
Berkay <yurdaguel.b@gmail.com>
2020-10-22 18:57:55 +0200

    implementing about section, adding me img

---
+++ src/components/landing-page/about/about.component.jsx
@@ -0,0 +1,101 @@
+import React from "react"
+import { graphql, useStaticQuery, Link } from "gatsby"
+import Image from "gatsby-image"
+
+//styles
+import * as Styled from "./about.styles"
+
+const query = graphql`
+  {
+    file(relativePath: { eq: "me.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+const About = () => {
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+  return (
+    <Styled.Container>
+      <Styled.Heading>About me</Styled.Heading>
+      <Styled.Content>
+        <Styled.RowOne>
+          <Styled.ImageContainer>
+            <Styled.ImageOverlayContainer>
+              <Styled.ImageOverlay></Styled.ImageOverlay>
+            </Styled.ImageOverlayContainer>
+            <Image fluid={fluid} />
+          </Styled.ImageContainer>
+          <ul>
+            <li>
+              <h2>What are my favourite places in Japan?</h2>
+              <p>
+                Vinyl cliche art party offal venmo. Hammock officia fixie,
+                jianbing heirloom nostrud sustainable. Occaecat viral
+                gluten-free roof party squid dolore.
+              </p>
+            </li>
+            <li>
+              <h2>Why I dont show my face?</h2>
+              <p>
+                Vinyl cliche art party offal venmo. Hammock officia fixie,
+                jianbing heirloom nostrud sustainable. Occaecat viral
+                gluten-free roof party squid dolore.
+              </p>
+            </li>
+            <li>
+              <h2>What are my must see suggestions for Japan?</h2>
+              <p>
+                Vinyl cliche art party offal venmo. Hammock officia fixie,
+                jianbing heirloom nostrud sustainable. Occaecat viral
+                gluten-free roof party squid dolore.
+              </p>
+            </li>
+          </ul>
+        </Styled.RowOne>
+        <Styled.RowTwo>
+          <ul>
+            <li>
+              <h2>Who I am?</h2>
+              <p>
+                Vinyl cliche art party offal venmo. Hammock officia fixie,
+                jianbing heirloom nostrud sustainable. Occaecat viral
+                gluten-free roof party squid dolore.
+              </p>
+            </li>
+            <li>
+              <h2>Which equipment do I use?</h2>
+              <p>
+                Vinyl cliche art party offal venmo. Hammock officia fixie,
+                jianbing heirloom nostrud sustainable. Occaecat viral
+                gluten-free roof party squid dolore.
+              </p>
+            </li>
+            <li>
+              <h2>Where are some unusual places i have been?</h2>
+              <p>
+                Vinyl cliche art party offal venmo. Hammock officia fixie,
+                jianbing heirloom nostrud sustainable. Occaecat viral
+                gluten-free roof party squid dolore.
+              </p>
+            </li>
+            <li>
+              <h2>What is next?</h2>
+              <Link to="/map">Explore Japan</Link>
+            </li>
+          </ul>
+        </Styled.RowTwo>
+      </Styled.Content>
+    </Styled.Container>
+  )
+}
+
+export default About
---
+++ src/components/landing-page/about/about.styles.jsx
@@ -0,0 +1,101 @@
+import styled from "styled-components"
+
+export const Container = styled.article`
+  height: 100%;
+  width: 100%;
+  max-width: 1500px;
+  padding: 0 50px;
+  margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+`
+
+export const Heading = styled.h2`
+  text-decoration: underline;
+  text-align: center;
+  font-size: 2rem;
+`
+
+export const Content = styled.section`
+  display: flex;
+  margin-top: 100px;
+
+  ul {
+    list-style: none;
+    margin: 0;
+
+    li {
+      margin-bottom: 75px;
+      h2 {
+        margin-bottom: 25px;
+      }
+      p {
+        line-height: 32px;
+      }
+    }
+  }
+`
+
+export const RowOne = styled.section`
+  height: 100%;
+  margin-right: 100px;
+
+  ul {
+    margin-top: 100px;
+  }
+`
+
+export const RowTwo = styled.section`
+  ul {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  & li:last-child {
+    text-align: center;
+    margin-top: auto;
+    margin-bottom: auto;
+  }
+
+  li {
+    a {
+      display: inline-block;
+      padding: 10px 15px;
+      border-radius: 5px;
+      border: 3px solid #bf0436;
+      color: #bf0436;
+      transition: all 0.3s ease-in;
+
+      &:hover {
+        background: #bf0436;
+        color: white;
+      }
+    }
+  }
+`
+
+export const ImageContainer = styled.section`
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  img {
+    border-radius: 5px;
+  }
+`
+
+export const ImageOverlayContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  z-index: 1;
+  padding: 10px;
+`
+
+export const ImageOverlay = styled.div`
+  width: 100%;
+  height: 100%;
+  border: 1px solid white;
+  border-radius: 5px;
+`
